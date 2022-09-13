ca0bea5b6de10e16e98f7bb76c671112c6bae0d5
said1854 <msaliogullari@gmail.com>
2022-09-13 14:12:09 +0300

    dotenv removed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const format = require('date-format');
-require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
